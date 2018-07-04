093ff8234a44247a5896fdd7e81c6bd1415d31ca
williamblair <blairw@cs.sunyit.edu>
2018-07-03 23:49:07 -0400

    self host image?

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ randomNames = [
     'sonny'
 ]
 
-tripleGayUrl = 'https://i.kym-cdn.com/photos/images/original/001/315/902/034.png'
+tripleGayUrl = 'http://people.sunyit.edu/~blairw/triplegay.png'
 
 # called whenever the bot recieves a POST request
 @app.route('/', methods=['POST'])
